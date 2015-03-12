70e9502a7bce595edf34a5111c8615d6993c0697
William De Vos <williamdevos@gmail.com>
2015-03-12 19:00:36 +0100

    Complete crud assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   
-  resources :posts
+  get 'advertisements/index'
+
+  get 'advertisements/show'
+
+  resources :advertisements
   
   get 'about' => 'welcome#about'
 

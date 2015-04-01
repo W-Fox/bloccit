ddf64cae6e89f2009499f76a19e3d8a7c0ccd901
William De Vos <williamdevos@gmail.com>
2015-04-01 17:09:56 +0200

    Associate & neste: bug post_controller update fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -37,7 +37,7 @@ class PostsController < ApplicationController
         authorize @post
       if @post.update_attributes(params.require(:post).permit(:title, :body))
          flash[:notice] = "Post was updated."
-         redirect_to @post
+         redirect_to [@topic, @post]
       else
          flash[:error] = "There was an error saving the post. Please try again."
          render :edit

0712be962ba89c87bcb6d019fd37e7726b480fbd
William De Vos <williamdevos@gmail.com>
2015-04-01 14:45:50 +0200

    Complete Authorization assignment

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -35,7 +35,7 @@ class ApplicationPolicy
   end
 
   def scope
-    record
+    record.class
   end
 
   class Scope
--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,5 +1,22 @@
 class PostPolicy < ApplicationPolicy
+  class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      if user.admin? || user.moderator?
+        scope.all
+      else scope.where(:id => user.id).exists?
+      end
+    end
+  end
+
+
   def index?
-    true
+    user.admin? || user.moderator? || user.id?
   end
 end

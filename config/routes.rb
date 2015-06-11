b8a0c1d928f3bc364c368083e5fc8c11e9f5af48
William De Vos <williamdevos@gmail.com>
2015-06-11 19:04:42 +0200

    complete public profiles checkpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Bloccit::Application.routes.draw do
 
   devise_for :users
-    resources :users, only: [:update]
+    resources :users, only: [:update, :show]
  
   resources :topics do
     resources :posts, except: [:index]
@@ -10,6 +10,7 @@ Bloccit::Application.routes.draw do
   resources :posts, only: [] do
    resources :comments, only: [:create, :destroy]
    resources :favorites, only: [:create, :destroy]
+   
    post '/up-vote' => 'votes#up_vote' , as: :up_vote
    post '/down-vote' => 'votes#down_vote', as: :down_vote
   end

004216aaf964c74737a5269890eb6a66ec600d62
William De Vos <williamdevos@gmail.com>
2015-06-11 19:22:10 +0200

    completed assignment public profile

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
  
+include Warden::Test::Helpers
+Warden.test_mode!
+ 
 describe "Visiting profiles" do
  
   include TestFactories
@@ -7,9 +10,10 @@ describe "Visiting profiles" do
   before do
     @user = authenticated_user
     @post = associated_post(user: @user)
-    @comment = Comment.new(user: @user, post: @post, body: "A Comment")
-    allow (@comment).to receive(:send_favorite_emails)
-    @comment.save!
+    @comment = @post.comments.build(user: @user, body: "A Comment")
+    allow(@comment).to receive(:send_favorite_emails)
+    @comment.save
+    @comment = comment_without_email
   end
  
   describe "not signed in" do
@@ -17,11 +21,26 @@ describe "Visiting profiles" do
     it "shows profile" do
       visit user_path(@user)
       expect(current_path).to eq(user_path(@user))
-
+   
       expect( page ).to have_content(@user.name)
       expect( page ).to have_content(@post.title)
       expect( page ).to have_content(@comment.body)
     end
+  end
  
+  describe "signed in" do
+ 
+    before do
+      login_as(@user, :scope => :user)
+    end
+ 
+    it "shows profile" do
+      visit user_path(@user)
+      expect(current_path).to eq(user_path(@user))
+ 
+      expect( page ).to have_content(@user.name)
+      expect( page ).to have_content(@post.title)
+      expect( page ).to have_content(@post.title)
+    end
   end
 end
